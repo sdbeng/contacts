fe66bb879e5796c6790c45f9a9b3d73372e84953
Sergio Barrera <admin32@Sergios-MacBook-Pro.local>
2017-07-03 07:24:16 -0700

    Controlled components

---
+++ controlled-components.js
@@ -0,0 +1,4 @@
+## Controlled Component
+Note that the value attribute is set on the <input> element. Our displayed value will always be the value in the component's state, making our state the - single source of truth.
+
+Because it is React that ultimately controls the value of our input form element, we consider this component a Controlled Component.
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,52 +1,69 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-//refactor into a functional component
-function ListContacts(props){
-  return (
-    <ol className='contact-list'>
-      {props.contacts.map((c) => (
-        <li key={c.id} className='contact-list-item'>
-          <div className='contact-avatar' style={{backgroundImage:`url(${c.avatarURL})`}}>
-          </div>
-          <div className='contact-details'>
-            <p>{c.name}</p>
-            <p>{c.email}</p>
-          </div>
-          <button onClick={() => props.onDeleteContact(c)} className='contact-remove'>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
-  )
-}
-
-// class ListContacts extends Component {
-//   render(){
-//     // console.log(this.props);
-//     return (
-//       <ol className='contact-list'>
-//         {this.props.contacts.map(c => (
-//           <li key={c.id} className='contact-list-item'>
-//             <div className='contact-avatar' style={{backgroundImage:`url(${c.avatarURL})`}}>
-//             </div>
-//             <div className='contact-details'>
-//               <p>{c.name}</p>
-//               <p>{c.email}</p>
-//             </div>
-//             <button className='contact-remove'>
-//               Remove
-//             </button>
-//           </li>
-//         ))}
-//       </ol>
-//     )
-//   }
+//refactor into a functional component - if neccesary
+// function ListContacts(props){
+//   return (
+//     <ol className='contact-list'>
+//       {props.contacts.map((c) => (
+//         <li key={c.id} className='contact-list-item'>
+//           <div className='contact-avatar' style={{backgroundImage:`url(${c.avatarURL})`}}>
+//           </div>
+//           <div className='contact-details'>
+//             <p>{c.name}</p>
+//             <p>{c.email}</p>
+//           </div>
+//           <button onClick={() => props.onDeleteContact(c)} className='contact-remove'>
+//             Remove
+//           </button>
+//         </li>
+//       ))}
+//     </ol>
+//   )
 // }
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+  state = {
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState({query: query.trim()})
+  }
+  render(){
+    // console.log(this.props);
+    return (
+      <div className="list-contacts">
+        <div>
+          <input
+          className="search-contacts"
+          type='text'
+          placeholder='Search Contacts'
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)} />
+        </div>
+        <ol className='contact-list'>
+          {this.props.contacts.map(c => (
+            <li key={c.id} className='contact-list-item'>
+              <div className='contact-avatar' style={{backgroundImage:`url(${c.avatarURL})`}}>
+              </div>
+              <div className='contact-details'>
+                <p>{c.name}</p>
+                <p>{c.email}</p>
+              </div>
+              <button className='contact-remove'>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
+  }
 }
+
+
 export default ListContacts
