1580fce1d6fb52443f6931fbb19e3f9c120672b7
Sergio Barrera <admin32@Sergios-MacBook-Pro.local>
2017-07-03 20:27:13 -0700

    fixed bug and add onDeleteContact to child component

--- controlled-components.js
+++ controlled-components.js
@@ -2,3 +2,33 @@
 Note that the value attribute is set on the <input> element. Our displayed value will always be the value in the component's state, making our state the - single source of truth.
 
 Because it is React that ultimately controls the value of our input form element, we consider this component a Controlled Component.
+
+##  Regular Expressions 💡
+In the previous video, we created a Regular Expression object and used that to test the format of the contacts' names. Regular expressions are too complex to get into in this program, but they're incredibly valuable in programming to verify patterns.
+
+Check out what MDN has to say about Regular Expressions. Also, check out how the String .match() method uses Regular expressions to verify patterns of text.
+
+## Destructuring objects
+At this point, our component is a bit unwieldy; the render() method accesses query from the state object (this.state.query) and contacts from the props object (this.props.contacts) quite often. Because props and state are just JavaScript objects, we can use an ES6 feature to unpack them into distinct variables rather than referencing them as this.state.query and this.props.contacts every time. This process of unpacking is called object destructuring.
+
+All in all, destructuring our objects shouldn't change the return value of our code, but it can make things look a bit cleaner.
+
+## Controlled Components Recap
+Controlled components refer to components that render a form, but the "source of truth" for that form state lives inside of the component state rather than inside of the DOM. The benefits of Controlled Components are:
+
+* instant input validation
+* conditionally disable/enable buttons
+* enforce input formats
+
+In our ListContacts component, not only does the component render a form, but it also controls what happens in that form based on user input. In this case, event handlers update the component's state with the user's search query. And as we've learned: any changes to React state will cause a re-render on the page, effectively displaying our live search results.
+
+## Putting it All Into Perspective
+When it comes to keeping track of data in your app, think about what will be done with that data, and what that data will look like as your user interfaces with your app. If you want your component to store mutable local data, consider using state to hold this information. Many times, it is state that will be used to manage controlled form elements in your components.
+
+On the other hand, if some information isn't expected to change over time, and is generally designed to be "read-only" throughout your app, consider using props instead. Both state and props will generally be in the form of an object, and changes in either will trigger a re-render of the component, but they each play very different roles in your app.
+
+We covered a lot in this lesson, and you've made great progress. Here are some resources that may help solidify some of these concepts:
+
+Thinking in React
+Functional Components vs. Stateless Functional Components vs. Stateless Components
+Controlled Components
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 //refactor into a functional component - if neccesary
 // function ListContacts(props){
@@ -33,8 +35,22 @@ class ListContacts extends Component {
   updateQuery = (query) => {
     this.setState({query: query.trim()})
   }
+  clearQuery = () => {
+    this.setState({query: ''})
+  }
   render(){
     // console.log(this.props);
+
+    //only show the contacts matched by the regexp or whatever the list were originally
+    let showingContacts
+    if(this.state.query){
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingContacts = this.props.contacts.filter((contact) => match.test((contact.name)))
+    }else {
+      showingContacts = this.props.contacts
+    }
+    // we also want to make sure our contacts are sorted alfa by name
+    showingContacts.sort(sortBy('name'))
     return (
       <div className="list-contacts">
         <div>
@@ -45,8 +61,17 @@ class ListContacts extends Component {
           value={this.state.query}
           onChange={(event) => this.updateQuery(event.target.value)} />
         </div>
+{/* add total contacts span div: if contacts shown are not the same as total contacts, then show up the following span div */}
+        {showingContacts.length !== this.props.contacts.length && (
+          <div className="showing-contacts">
+            <span>Now showing {showingContacts.length} of {this. props.contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
-          {this.props.contacts.map(c => (
+          {/* instead of mapping over this.props.contacts, map over showingContacts */}
+          {showingContacts.map(c => (
             <li key={c.id} className='contact-list-item'>
               <div className='contact-avatar' style={{backgroundImage:`url(${c.avatarURL})`}}>
               </div>
@@ -54,7 +79,7 @@ class ListContacts extends Component {
                 <p>{c.name}</p>
                 <p>{c.email}</p>
               </div>
-              <button className='contact-remove'>
+              <button onClick={() => this.props.onDeleteContact(c)} className='contact-remove'>
                 Remove
               </button>
             </li>
